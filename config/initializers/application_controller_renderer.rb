f00e08b15c5cfb0d579fb455f916c6b700cee8be
vagrant <vagrant@localhost.localdomain>
2020-07-02 06:31:36 +0000

    [Finish]5,4

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '7b68a578c4b8286cb6665e62cc15803fa9d17e3c5386cb13648262e740f0bac9fba4cd96efa0f0c8193b569e55c0d95e3d34b2f8e15e8b06ec1acd9fe108786a'
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
 
   root 'post_images#index'
 
-  resources :post_images, only: [:new, :create, :index, :show]
+  resources :post_images, only: [:new, :create, :index, :show] do
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+
+  end
 end
